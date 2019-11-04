376f3975408dac74f78002760a52473fe42fb65f
Abhinav Saxena <abhinav.saxena@invenio-solutions.com>
2019-11-04 04:45:59 +0000

    1st commit

---
+++ webapp/controller/View2.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("month_display.demo_month_2.controller.View2", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"month_display/demo_month_2/test/unit/controller/View2.controller"
+], function () {
+	"use strict";
+});
